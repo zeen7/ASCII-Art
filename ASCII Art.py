1db17d2a1e3261af8c8098dba042913f29773745
Zeen W <zeen-wang@hotmail.com>
2020-01-27 17:44:17 -0500

    Added File chooser

--- ASCII Art.py
+++ ASCII Art.py
@@ -3,38 +3,38 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 #File chooser dialog window
-Tk().withdraw() #gets rid of root window
-filename = askopenfilename() 
+try:    
+        Tk().withdraw() #gets rid of root window
+        filename = askopenfilename(title = "Select picture",filetypes = (("PNG","*.png"), ("JPEG","*.jpeg"),("All Files", "*.*")) )
+        #loads image
+        im = Image.open(filename).convert('RGB')
 
-<<<<<<< HEAD
-#loads image
-im = Image.open(filename).convert('RGB')
-=======
-#resize image to fit
-im=im.resize((round(im.width/4), round(im.height/4)))
->>>>>>> 064709a3820af377886174e29f2e4badbf06f4c8
+        #2D array setup
+        cols=im.height+1
+        rows=im.width+1
+        arr=[[0]*cols]*rows
 
-#2D array setup
-cols=im.height+1
-rows=im.width+1
-arr=[[0]*cols]*rows
+        #storing tuples of each pixel of the picture in 2D array
+        for y in range(im.height):
+                print("\n")
+                for x in range(im.width):
+                        arr[x][y]=im.getpixel((x,y))
 
-#storing tuples of each pixel of the picture in 2D array
-for y in range(im.height):
-    print("\n")
-    for x in range(im.width):
-        arr[x][y]=im.getpixel((x,y))
+                        #convert RGB tuples of each pixel to brightness values
+                        temp_tuple=arr[x][y]
+                        brightness_value=0.21*temp_tuple[0] + 0.72*temp_tuple[1] + 0.07*temp_tuple[2] #take a weighted average of the R, G and B values to account for human perception
+                        
+                        #convert brightness values to ASCII characters
+                        ascii_list="`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$" 
+                        ascii_index=(round(brightness_value/3.98))
+                        symbol=ascii_list[ascii_index]
 
-        #convert RGB tuples of each pixel to brightness values
-        temp_tuple=arr[x][y]
-        brightness_value=0.21*temp_tuple[0] + 0.72*temp_tuple[1] + 0.07*temp_tuple[2] #take a weighted average of the R, G and B values to account for human perception
-        
-        #convert brightness values to ASCII characters
-        ascii_list="`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$" 
-        ascii_index=(round(brightness_value/3.98))
-        symbol=ascii_list[ascii_index]
+                        #prints ASCII characters
+                        print(symbol, end="")
+                        print(symbol, end="")
+                        print(symbol, end="")
+except:
+        print("Error, please try again.")
 
-        #prints ASCII characters
-        print(symbol, end="")
-        print(symbol, end="")
-        print(symbol, end="")
+
+
